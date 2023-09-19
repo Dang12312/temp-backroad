86fbb3893d10bfacad03b9bdc0fa4de7a4088bd1
Dang12312 <80524787+Dang12312@users.noreply.github.com>
2023-09-19 09:54:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from './components/About'
+import Footer from './components/footer'
+import Hero from './components/hero'
+import Navbar from './components/Navbar'
+import Service from './components/service'
+import Tours from './components/tours'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Service />
+      <Tours />
+      <Footer />
+    </>
+  )
+}
+
+export default App
